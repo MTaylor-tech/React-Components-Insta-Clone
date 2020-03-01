2bfffcee562918d1984addd8e4161815623cd382
Matthew Taylor <majikpig@gmail.com>
2020-02-29 19:26:24 -0500

    likes and comments working

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -1,16 +1,32 @@
 // You will add code to this file
-import React, { useState } from "react";
+import React, { useState, createRef } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  const [userCommentText, setUserCommentText] = useState();
+  const [userComment, setUserComment] = useState({username:'', text:''});
+
+  const editComment = (event) => {
+    event.preventDefault();
+    setUserCommentText(event.target.value);
+  };
+
+  const addComment = (event) => {
+    event.preventDefault();
+    const newComment = {text: userCommentText, username: 'User' };
+    setUserComment(newComment);
+  }
+
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {props.comments.map(item=>{return <Comment comment={item} />})}
+      {userComment===undefined?'':<Comment comment={userComment} />}
+      <CommentInput changeComment={editComment} submitComment={addComment} comment={userCommentText} />
     </div>
   );
 };
